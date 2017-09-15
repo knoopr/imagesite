12419ad3ed719839295e63689cccbf2de1b0da7e
Rick Knoop <knoop.rick@gmail.com>
2017-09-15 09:13:22 -0400

    arrows and escape for display modal

--- photography/static/display.modal.js
+++ photography/static/display.modal.js
@@ -37,13 +37,13 @@ function showImageModal(parent) {
 
     var photographer = parent.getAttribute('photographer')
     var contact_email = parent.getAttribute('contactemail')
-    var tags = parent.getAttribute('tags')
+    //var tags = parent.getAttribute('tags')
     var image = parent.getAttribute('src')
 
-    if (tags.indexOf("'") > -1) {
+    /*if (tags.indexOf("'") > -1) {
         tags = tags.split("'").join('"');
     }
-    tags = JSON.parse(tags);
+    tags = JSON.parse(tags);*/
 
     document.getElementById('photographer-link').innerText = photographer;
     document.getElementById('photographer-link').setAttribute('href', "mailto:" + contact_email);
@@ -57,4 +57,42 @@ function showImageModal(parent) {
     sizeImageModal();
 }
 
+function changeImage(event){
+    var modal = document.getElementById('display-modal');  
+    if (modal.classList.contains('active')){
+        var key = event.keyCode;
+        
+        if (key == 27){
+            hideModal('display-modal')
+        }else if (key == 37){
+            var src = document.getElementById('modal-image').getAttribute('src')
+            var grid_elem = document.getElementById('grid-container').getElementsByClassName('grid')[0];
+            var node = grid_elem.querySelector('[src="' + src + '"]').parentNode
+            var parent = node.parentNode
+            var i = 0;
+            var index = Array.prototype.indexOf.call(parent.children, node);
+            if (index != 0){
+                showImageModal(parent.children[index-1].children[0])
+            }
+            else{
+                showImageModal(parent.children[parent.children.length-1].children[0])
+            }
+        }else if (key == 39){
+            var src = document.getElementById('modal-image').getAttribute('src')
+            var grid_elem = document.getElementById('grid-container').getElementsByClassName('grid')[0];
+            var node = grid_elem.querySelector('[src="' + src + '"]').parentNode
+            var parent = node.parentNode
+            var i = 0;
+            var index = Array.prototype.indexOf.call(parent.children, node);
+            if (index != (parent.children.length-1)){
+                showImageModal(parent.children[index+1].children[0])
+            }
+            else{
+                showImageModal(parent.children[0].children[0])
+            }
+        }
+    }
+}
+
+document.addEventListener("keydown",changeImage);
 window.addEventListener("resize", sizeImageModal);
